f0e1ed1e8d1521abf9d00636bfa76e893280e4c5
aonoexercist <xercisthegreat@gmail.com>
2026-03-22 17:14:39 +0800

    initialize database and update router tags in main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+from app.database.base import Base
+from app.database.connection import engine
 from app.routers import todo
 
 app = FastAPI()
 
-app.include_router(todo.router, prefix="/todos", tags=["todos"])
+Base.metadata.create_all(bind=engine)
 
-@app.get("/")
-def root():
-    return {"message": "Todo API is running 🚀"}
+app.include_router(todo.router, prefix="/todos", tags=["Todos"])
+
+
+# @app.get("/")
+# def root():
+#     return {"message": "API is running 🚀"}
