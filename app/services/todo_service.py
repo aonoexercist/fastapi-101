4fad54ffae66ce2a75939e899ad5f5d07987172d
aonoexercist <xercisthegreat@gmail.com>
2026-03-22 17:15:14 +0800

    refactor todo service to use SQLAlchemy for CRUD operations and remove in-memory storage

--- app/services/todo_service.py
+++ app/services/todo_service.py
@@ -1,16 +1,16 @@
-from app.schemas.todo import Todo
+from sqlalchemy.orm import Session
+from app.models.todo import TodoModel
+from app.schemas.todo import TodoCreate
 
-todos = []
+def create_todo(db: Session, todo: TodoCreate):
+    db_todo = TodoModel(**todo.model_dump())
+    db.add(db_todo)
+    db.commit()
+    db.refresh(db_todo)
+    return db_todo
 
-def create_todo(todo: Todo):
-    todos.append(todo)
-    return todo
+def get_all(db: Session):
+    return db.query(TodoModel).all()
 
-def get_all():
-    return todos
-
-def get_one(todo_id: int):
-    for t in todos:
-        if t.id == todo_id:
-            return t
-    return None
+def get_one(db: Session, todo_id: int):
+    return db.query(TodoModel).filter(TodoModel.id == todo_id).first()
