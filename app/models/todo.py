c6085c48661c43a622dcce0f0d9d3b6310ecccf6
aonoexercist <xercisthegreat@gmail.com>
2026-03-22 17:15:02 +0800

    add TodoModel SQLAlchemy model for managing todos

--- app/models/todo.py
+++ app/models/todo.py
@@ -1,1 +1,9 @@
-# placeholder for SQLAlchemy model
+from sqlalchemy import Column, Integer, String, Boolean
+from app.database.base import Base
+
+class TodoModel(Base):
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    completed = Column(Boolean, default=False)
