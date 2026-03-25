c95d1f2837f9fc9ee8b4933a42c554e1550e27ef
aonoexercist <xercisthegreat@gmail.com>
2026-03-25 10:39:49 +0800

    implement update and delete functions for todos in todo_service

--- app/services/todo_service.py
+++ app/services/todo_service.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from app.models.todo import TodoModel
-from app.schemas.todo import TodoCreate
+from app.schemas.todo import TodoCreate, TodoUpdate, Todo
 
 def create_todo(db: Session, todo: TodoCreate):
     db_todo = TodoModel(**todo.model_dump())
@@ -14,3 +14,31 @@ def get_all(db: Session):
 
 def get_one(db: Session, todo_id: int):
     return db.query(TodoModel).filter(TodoModel.id == todo_id).first()
+
+def update_todo(db: Session, todo_id: int, updated: TodoUpdate):
+    todo = db.query(TodoModel).filter(TodoModel.id == todo_id).first()
+
+    if not todo:
+        return None
+
+    if updated.title is not None:
+        todo.title = updated.title
+
+    if updated.completed is not None:
+        todo.completed = updated.completed
+
+    db.commit()
+    db.refresh(todo)
+
+    return todo
+
+def delete_todo(db: Session, todo_id: int):
+    todo = db.query(TodoModel).filter(TodoModel.id == todo_id).first()
+
+    if not todo:
+        return None
+
+    db.delete(todo)
+    db.commit()
+
+    return True
