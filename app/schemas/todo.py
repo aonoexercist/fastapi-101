e3b079c99bea1e857232d57d971e85cbfaedabbb
aonoexercist <xercisthegreat@gmail.com>
2026-03-25 10:39:44 +0800

    add schemas for updating and deleting todos

--- app/schemas/todo.py
+++ app/schemas/todo.py
@@ -1,7 +1,15 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class TodoCreate(BaseModel):
     title: str
+
+class TodoUpdate(BaseModel):
+    title: Optional[str] = None
+    completed: Optional[bool] = None
+
+class TodoDelete(BaseModel):
+    id: int
 
 class Todo(TodoCreate):
     id: int
